d51ad57da6bb2c5d9ed1448e0e451e0d4e4d650a
Sean M Hamlet <seanmhamlet@gmail.com>
2017-04-27 15:30:55 -0400

    Adds adding javascript as separate file notes

---
+++ 8-JavaScript/85-Separate_File/script.js
@@ -0,0 +1,12 @@
+var firstName = prompt("What is your first name?");
+var lastName = prompt("What is your last name?");
+var age = prompt("How old are you?");
+
+console.log("Your full name is " + firstName + " " + lastName);
+console.log("You are " + age + " years old");
+
+
+// var x = prompt("Give me the first number")
+// var y = prompt("Give me the second number")
+// var result = Number(x) + Number(y);
+// console.log(result)
